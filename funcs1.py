d6a8f5bc72709853eef0ff7a13393e27fb739a75
Dell-PC <Igorek.kovalenko.2015@mail.ru>
2023-06-02 20:15:04 +0300

    commit 3

--- funcs1.py
+++ funcs1.py
@@ -7,3 +7,13 @@ num1 = 15
 num2 = 20
 
 print("The sum is", add_numbers(num1, num2))
+
+def add_num(x, y):
+   num = x - y
+   return num
+
+
+num1 = 15
+num2 = 20
+
+print("The sum is", add_num(num1, num2))
